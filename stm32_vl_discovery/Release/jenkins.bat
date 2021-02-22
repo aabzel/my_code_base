4c0f99773696b7d997a08f81a317af85d3594353
aabdev <aabzel@yandex.ru>
2021-02-22 05:50:51 +0300

    + stm32F100 project

---
+++ stm32_vl_discovery/Release/jenkins.bat
@@ -0,0 +1,3 @@
+echo %PATH%
+make clean
+make all
---
+++ stm32_vl_discovery/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 21, 2021
+ *      Author: aabdev
+ */
+#include "stm32f1xx.h"
+
+int main() {
+    HAL_Init();
+
+    while(1);
+
+    return 0;
+}
